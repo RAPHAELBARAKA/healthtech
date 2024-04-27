07d2565350c0de63e3ab8d994d32555248f0d6df
Baraka <raphaelbaraka424@gmail.com>
2024-04-27 23:27:57 +0300

    sm

--- client-side/src/Components/RegisterForm.jsx
+++ client-side/src/Components/RegisterForm.jsx
@@ -39,7 +39,7 @@ function RegisterForm() {
       // Reset error state
       setError('');
     
-      const response = await axios.post("https://healthtech-s2ay.vercel.app/", {
+      const response = await axios.post("https://healthtech-s2ay.vercel.app/register", {
         name,
         email,
         password
