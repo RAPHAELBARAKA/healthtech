9cfb8f9b5af2c09c8bca9a95f8e7c6d41971b73d
RAPHAELBARAKA <153984597+RAPHAELBARAKA@users.noreply.github.com>
2024-04-28 15:15:57 +0300

    Update RegisterForm.jsx

--- client-side/src/Components/RegisterForm.jsx
+++ client-side/src/Components/RegisterForm.jsx
@@ -39,7 +39,7 @@ function RegisterForm() {
       // Reset error state
       setError('');
   
-      const response = await axios.post("https://healthtech-k2dx.vercel.app/", {
+      const response = await axios.post("https://healthtech-k2dx.vercel.app/register", {
         name,
         email,
         password
