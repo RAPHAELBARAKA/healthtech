e479f9dbd3fc9fdbd3cf7f5804c0d2b3a13a78a0
RAPHAELBARAKA <153984597+RAPHAELBARAKA@users.noreply.github.com>
2024-04-28 14:12:18 +0300

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -14,13 +14,11 @@ const AppointmentController = require('./Controller/AppointmentController');
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://healthtech.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors({
+  origin: 'https://healthtech-h9b4jbfjb-barakas-projects-88806555.vercel.app',
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
 // Initialize session middleware
 app.use(session({
   secret: 'your-secret-key',
