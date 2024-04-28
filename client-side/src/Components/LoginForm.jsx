3823033efb3c479635031f05422ebdbae960d06a
RAPHAELBARAKA <153984597+RAPHAELBARAKA@users.noreply.github.com>
2024-04-28 13:11:59 +0300

    Update LoginForm.jsx

--- client-side/src/Components/LoginForm.jsx
+++ client-side/src/Components/LoginForm.jsx
@@ -20,7 +20,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:3000/login', {
+      const response = await axios.post('https://healthtech-s2ay.vercel.app/login', {
         email,
         password
       });
